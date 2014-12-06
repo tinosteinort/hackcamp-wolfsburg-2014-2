c3056fb6a9ce08fd0c69fe8db168eda32d00ee6c
Tino Steinort <tinosteinort@live.de>
2014-12-06 16:17:42 +0100

    flush after save() add find Method

--- src/main/java/de/wolfsburg/hackcamp/business/blog/persistence/BlogPostRepository.java
+++ src/main/java/de/wolfsburg/hackcamp/business/blog/persistence/BlogPostRepository.java
@@ -1,12 +1,14 @@
 package de.wolfsburg.hackcamp.business.blog.persistence;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
 import de.wolfsburg.hackcamp.business.blog.persistence.entity.BlogPost;
-import de.wolfsburg.hackcamp.business.blog.persistence.entity.Category;
 
 @Repository
 public class BlogPostRepository {
@@ -14,10 +16,11 @@ public class BlogPostRepository {
 	EntityManager em;
 
 	public BlogPost save(BlogPost post) {
-		if (post.getId() == null) {
+		if (post.getId() != null) {
 			return em.merge(post);
 		}
 
+		post.setCreatedOn(LocalDateTime.now());
 		em.persist(post);
 
 		return post;
@@ -31,5 +34,10 @@ public class BlogPostRepository {
 	public Long countAll() {
 		return em.createQuery("SELECT COUNT(bp) FROM BlogPost bp", Long.class)
 				.getSingleResult().longValue();
+	}
+	
+	@SuppressWarnings("unchecked")
+    public List<BlogPost> findBlogPosts() {
+	    return em.createQuery("FROM BlogPost bp").getResultList();
 	}
 }
